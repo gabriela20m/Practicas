be8f1e9888634818ce910280c17a5509b90c400d
gaby06M <gabrielamunozmartinez1@gmail.com>
2024-08-09 21:59:04 -0500

    practicas

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,10 @@
-// scripts.js
-
 function showAlert() {
     alert('¡Gracias por visitar mi página!');
 }
 
-// Ejemplo de función para alternar la visibilidad de las listas
 function toggleList(id) {
     const element = document.getElementById(id);
-    if (element.style.display === 'none') {
+    if (element.style.display === 'none' || element.style.display === '') {
         element.style.display = 'block';
     } else {
         element.style.display = 'none';
